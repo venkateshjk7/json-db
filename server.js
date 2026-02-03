6b16adad8998616eb6cea3d733ced899d588a99a
venkateshjk7 <venkatesh2005jk@gmail.com>
2026-02-03 05:44:01 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 10000;
+server.listen(PORT);
